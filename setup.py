b5944741e85dd6d77ae543af76b823d547ff0f18
Ian Good <icgood@gmail.com>
2015-09-22 15:05:52 -0400

    advertise support for py3.5

--- setup.py
+++ setup.py
@@ -42,4 +42,5 @@ setup(name='pysasl',
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.4'])
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5'])
