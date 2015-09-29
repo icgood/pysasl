c12a245df46e4eae1b0e02f8c50c1d529bf9e08f
Ian Good <icgood@gmail.com>
2015-09-28 22:31:17 -0400

    set version 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pysasl',
-      version='0.1.1',
+      version='0.2.0',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Pure Python SASL client and server library.',
