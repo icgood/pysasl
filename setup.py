bf4bb91e6cdb6a5e9db24288fb3b510e92ab437f
Ian Good <ian@icgood.net>
2020-11-21 13:25:33 -0500

    Add Python 3.9 to the supported list

--- setup.py
+++ setup.py
@@ -55,4 +55,5 @@ setup(name='pysasl',
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
-                   'Programming Language :: Python :: 3.8'])
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9'])
