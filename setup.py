713adac57feac9a404762e81e86433f4f0160b6c
Ian Good <ian@icgood.net>
2022-09-17 13:47:46 -0400

    Set to 1.0.0.rc1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('LICENSE.md') as fh:
     license = fh.read()
 
 setup(name='pysasl',
-      version='0.11.0',
+      version='1.0.0.rc1',
       author='Ian Good',
       author_email='ian@icgood.net',
       description='Pure Python SASL client and server library.',
